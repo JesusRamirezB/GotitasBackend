69bfca206f8f17f4e810a69d220bf60ee3fc1759
Jesus <jesus.ramirez.bocanegra@gmail.com>
2025-09-12 23:39:22 -0600

    Revert "added User and payment endpoint"

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { PaymentsModule } from './payments/payments.module';
 
 @Module({
-  imports: [UsersModule, PaymentsModule],
+  imports: [],
   controllers: [AppController],
   providers: [AppService],
 })
